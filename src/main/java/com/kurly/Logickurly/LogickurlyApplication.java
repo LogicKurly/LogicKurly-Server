8f036dd602b5766f5b4820d849270806eb0c0c11
Sangjin <sangjin170202@naver.com>
2022-09-24 10:28:01 +0900

    chore: project setting

---
+++ src/main/java/com/kurly/Logickurly/LogickurlyApplication.java
@@ -0,0 +1,13 @@
+package com.kurly.Logickurly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogickurlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogickurlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kurly/Logickurly/LogickurlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurly.Logickurly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogickurlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
